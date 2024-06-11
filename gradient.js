6949289fa6ba9c972b4c018f76b6b9700615c26c
radjah <68883593+radjahfromdiscord@users.noreply.github.com>
2024-06-12 00:27:09 +0200

    Update gradient.js

--- gradient.js
+++ gradient.js
@@ -1,4 +1,5 @@
-//Converting colors to proper format
+class Gradient {
+    //Converting colors to proper format
 function normalizeColor(hexCode) {
     return [(hexCode >> 16 & 255) / 255, (hexCode >> 8 & 255) / 255, (255 & hexCode) / 255]
   } ["SCREEN", "LINEAR_LIGHT"].reduce((hexCode, t, n) => Object.assign(hexCode, {
@@ -508,10 +509,5 @@ function normalizeColor(hexCode) {
   * Gradient.toggleColor(index)
   * Gradient.updateFrequency(freq)
   */
-  
-  
-  
-  
-  
-  
-  
+
+}
